61fd5f717e8fafc46dc74ecd2753a5f8d8079511
Nick Meinhold <nick@enspyr.co>
2023-08-04 01:23:20 +1000

    Handle KeyEvents, add CustomPaint to paint text

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MainApp());
+final codeValeNotifier = ValueNotifier<String>('');
+
+const textStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 30,
+);
+
+bool keyEventHandler(KeyEvent event) {
+  // switch case
+  print(event.character);
+  if (event.character != null) {
+    codeValeNotifier.value += event.character!;
+  }
+
+  // indicates whether Flutter "handles" the event
+  // TODO: we should return false for (eg.) Cmd-Q
+  return true;
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: HomeScreen(),
       ),
     );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    HardwareKeyboard.instance.addHandler(keyEventHandler);
+  }
+
+  @override
+  void dispose() {
+    HardwareKeyboard.instance.removeHandler(keyEventHandler);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CustomPaint(
+        size: const Size(300, 300),
+        painter: MyPainter(),
+      ),
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  MyPainter() : super(repaint: codeValeNotifier);
+  @override
+  void paint(Canvas canvas, Size size) {
+    final textSpan = TextSpan(
+      text: codeValeNotifier.value,
+      style: textStyle,
+    );
+
+    final textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+    );
+
+    textPainter.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+    final xCenter = (size.width - textPainter.width) / 2;
+    final yCenter = (size.height - textPainter.height) / 2;
+    final offset = Offset(xCenter, yCenter);
+    textPainter.paint(canvas, offset);
+  }
+
+  @override
+  // From docs: called when a new instance of the class is provided, to check if the new instance actually represents different information.
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
